e4da38d5d85825720d25924fc4bbe1142f6550d0
Diego Ricardo <diegoricardoweb@gmail.com>
2025-02-06 15:23:44 -0300

    feat: update page info plans

--- front/src/view/pages/PlansInfo/index.tsx
+++ front/src/view/pages/PlansInfo/index.tsx
@@ -1,50 +1,62 @@
-import { Spinner } from "@/view/components/Spinner"
-import { Card, CardContent, CardHeader, CardTitle } from "@/view/components/ui/card"
-import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/view/components/ui/table"
-import { BreadcrumbPlan } from "./components/BreadcrumbPlan"
-import { PlanItem } from "./components/PlanItem"
-import { usePlansInfoController } from "./usePlansInfoController"
+import { Whatsapp2 } from "@/view/components/icons/Whatsapp2";
+import { Button } from "@/view/components/ui/button";
+import { Card, CardContent } from "@/view/components/ui/card";
+import { Link } from "react-router-dom";
+import { BreadcrumbPlan } from "./components/BreadcrumbPlan";
 
 export default function PlansInfo() {
-  const { plans, isLoading } = usePlansInfoController();
-
   return (
     <>
       <BreadcrumbPlan />
 
       <div>
         <Card className="min-h-[500px] relative">
-          {isLoading && (
-            <div className="w-full h-full flex justify-center items-center absolute top-0 left-0">
-              <Spinner className="w-6 h-6 fill-primary" />
-            </div>
-          )}
 
-          <CardHeader>
-            <CardTitle>Planos</CardTitle>
-          </CardHeader>
           <CardContent>
-            {!isLoading && (
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead className="w-[230px]">Nome</TableHead>
-                    <TableHead>Atualizações</TableHead>
-                    <TableHead>Mídia Digital</TableHead>
-                    <TableHead>Impresso</TableHead>
-                    <TableHead>Apresentações</TableHead>
-                    <TableHead>Vídeos</TableHead>
-                    <TableHead className="w-[130px]" />
-                  </TableRow>
-                </TableHeader>
+            <div className="flex justify-center mt-10 mb-10">
+              <Button size="lg" className="bg-green-500 hover:bg-green-600">
+                <Link
+                  to={`https://api.whatsapp.com/send/?phone=5541996777195&text=Quero quero mais informações sobre os planos.&type=phone_number&app_absent=0`}
+                  className="flex items-center gap-1"
+                  target="_blank"
+                >
+                  <Whatsapp2 className="w-6 h-6" /> Contratar
+                </Link>
+              </Button>
+            </div>
 
-                <TableBody>
-                  {plans?.map((plan) => (
-                    <PlanItem key={plan.id} {...plan} />
-                  ))}
-                </TableBody>
-              </Table>
-            )}
+            <div className="text-center mb-10">
+              <p>
+                Com a Inovasite, você tem o poder de escolher <strong>quando</strong> e <strong>como</strong> quer suas artes. Nossos pacotes oferecem total flexibilidade: você pode enviar suas solicitações sob demanda ou optar por planos onde cuidamos de toda a gestão e criação para você.
+              </p><br/>
+
+              <p>
+                Artes <strong>100% exclusivas</strong>, criadas por profissionais talentosos, sem modelos prontos ou designs repetidos. Cada detalhe é pensado para atender às suas necessidades e superar suas expectativas.
+              </p><br/>
+
+              <p>
+                Seja para sua marca, projetos pessoais ou campanhas especiais, nós transformamos suas ideias em realidade. 💡
+              </p><br/>
+
+              <p><strong>Pronto para elevar sua criatividade?</strong> Escolha seu plano e deixe a mágica acontecer! 😉</p>
+            </div>
+
+            <iframe
+              src="https://inovasite.com/planos.pdf"
+              className="w-full h-[1000px]"
+            />
+
+            <div className="flex justify-center mt-4">
+              <Button size="lg" className="bg-green-500 hover:bg-green-600">
+                <Link
+                  to={`https://api.whatsapp.com/send/?phone=5541996777195&text=Quero quero mais informações sobre os planos.&type=phone_number&app_absent=0`}
+                  className="flex items-center gap-1"
+                  target="_blank"
+                >
+                  <Whatsapp2 className="w-6 h-6" /> Contratar
+                </Link>
+              </Button>
+            </div>
           </CardContent>
         </Card>
       </div>
